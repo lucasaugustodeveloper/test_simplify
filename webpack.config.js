9344d9232b291ffd1eb7f72e26325369f96f95d5
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-08 20:46:42 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 8080,
+        hot: true
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
