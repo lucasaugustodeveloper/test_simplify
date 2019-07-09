5f9c84c4303fe65e866cb0af2a61c81c9343b82c
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-08 22:37:32 -0300

    add redux and libs for request with promisse

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './App/App'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+import reducers from './main/reducer'
+
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers)
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>
+  , document.querySelector('#root'))
 
 module.hot.accept()
