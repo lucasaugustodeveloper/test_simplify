7f9401a3ae56ed576fb81499649146243f2112e5
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-09 23:48:37 -0300

    change method search for search tag

--- src/Header/action.js
+++ src/Header/action.js
@@ -1,24 +1,23 @@
 // import { search as axios } from '../services/api/flickr'
 import axios from 'axios'
 
-const url = 'http://www.mocky.io/v2/5d23eb882f000051002415e4'
+const instance = axios.create({
+  baseURL: 'https://api.unsplash.com/search/photos',
+  headers: {
+    'Authorization': 'Client-ID ac0f01b804cf94e9188c2a3a9f8c82fb3fd89fe247c91643eea356146d95a61d'
+  },
+})
 
 export const changeText = event => ({
   type: 'TEXT_CHANGED',
   payload: event.target.value
 })
 
-export const search = () => {
-  const request = axios.get(url)
-
-    return {
-      type: 'TEXT_SEARCHED',
-      payload: request,
-    }
-  // return (dispatch, getState) => {
-  //   const text = getState().photos.text
-  //   const search = text ? `?query=${text}` : ''
-  //   const request = axios.get(search)
-  //     .then(res => dispatch({ type: 'TEXT_SEARCHED', payload: res.data }))
-  // }
+export const search = text => {
+  return (dispatch, getState) => {
+    const textState = getState().photos.text
+    const search = textState ? `?query=${textState}` : text
+    const request = instance.get(search)
+      .then(res => dispatch({ type: 'TODO_SEARCHED', payload: res }))
+  }
 }
