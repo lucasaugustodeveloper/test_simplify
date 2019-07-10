9fb5ef16a4b4931f611a1beb5e8fe9d9adec32dd
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-09 23:48:04 -0300

    change functional component for class component

--- src/Header/HeaderForm.jsx
+++ src/Header/HeaderForm.jsx
@@ -33,7 +33,7 @@ class AppForm extends Component {
         />
         <button
           className="AppForm-btn"
-          onClick={search}
+          onClick={() => search(text)}
         >
           search
     </button>
@@ -41,25 +41,6 @@ class AppForm extends Component {
     )
   }
 }
-
-// const AppForm = ({ text, changeText, search }) => (
-  // <header className="AppForm-header col-lg-12 center-xs">
-  //   <input
-  //     type="text"
-  //     id="text"
-  //     className="AppForm-search"
-  //     value={ text }
-  //     onChange={ changeText }
-  //     placeholder="enter your search"
-  //   />
-  //   <button
-  //     className="AppForm-btn"
-  //     onClick={ search }
-  //   >
-  //     search
-  //   </button>
-  // </header>
-// )
 
 const mapStateToProps = state => ({ text: state.photos.text })
 const mapDispatchToProps = dispatch =>
